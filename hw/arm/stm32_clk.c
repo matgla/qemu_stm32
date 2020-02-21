bdb9f51faefe0f1c21526044c6e29245715a53c7
Mateusz Stadnik <matgla@live.com>
2020-02-21 15:59:57 +0100

    clock implementation started

--- hw/arm/stm32_clk.c
+++ hw/arm/stm32_clk.c
@@ -24,19 +24,93 @@
 
 #include <stdint.h>
 
-struct Stm32ClockTree
+#include "hw/irq.h"
+
+struct Stm32Clock
 {
 public:
     const char *name;
 
     /* parameters */
-    uint32_t input_freqency;
-    uint32_t output_freqency;
-    uint32_t max_output_frequency;
-
+    uint64_t input_freqency;
+    uint64_t output_freqency;
+    uint64_t max_output_frequency;
+    uint64_t multiplier;
+    uint64_t divisor;
 
     /* state */
     int enabled;
+
+    uint32_t number_of_observers;
+    qemu_irq* observers;
+
+    uint32_t number_of_inputs;
+    Stm32Clock* inputs;
+
+    uint32_t number_of_outputs;
+    Stm32Clock* outputs;
 };
+
+void stm32_process_clock_parameters_change(Stm32Clock* self)
+{
+    uint64_t new_output_frequency = 0;
+
+    if (self->enabled)
+    {
+        new_output_frequency = self->input_freqency * self->multiplier / self->divisor;
+    }
+
+    if (self->output_freqency != new_output_frequency)
+    {
+        if (new_output_frequency > self->max_output_frequency)
+        {
+            // SOME ERROR handling procedure
+        }
+        self->output_freqency = new_output_frequency;
+        stm32_notify_frequency_change(self);
+        stm32_propagate_to_child_clocks(self);
+    }
+
+
+}
+
+void stm32_change_clock_input_frequency(Stm32Clock* self, uint32_t new_frequency)
+{
+    self->input_freqency = new_frequency;
+
+    stm32_process_clock_parameters_change(self);
+}
+
+void stm32_notify_frequency_change(Stm32Clock* self)
+{
+    int i;
+    for (i = 0; i < self->number_of_observers; ++i)
+    {
+        qemu_set_irq(self->observers[i], 1);
+    }
+}
+
+Stm32Clock* stm32_get_selected_input(Stm32Clock* self)
+{
+
+}
+
+void stm32_propagate_to_child_clocks(Stm32Clock* self)
+{
+    int i;
+    Stm32Clock *next, *input;
+
+    for (i = 0; i < self->number_of_outputs; ++i)
+    {
+        next = &self->outputs[i];
+        input = stm32_get_selected_input(next);
+
+        if (input == self)
+        {
+            stm32_change_clock_input_frequency(next, self->output_freqency);
+        }
+    }
+}
+
 
 
